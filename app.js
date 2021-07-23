7a92821398a2d66ac76045a99f4352a1dabe7cd7
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2021-07-23 16:58:30 -0400

    0.7

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ add.addEventListener("click", e => {
     let todoMonth = form.children[1].value;
     let todoDate = form.children[2].value;
 
+    if ( todoText === "") {
+        alert("please enter text");
+        return;
+    }
+
     //  create an todo item
     let addTodo = document.createElement("div");
     addTodo.classList.add("todo");
@@ -22,8 +27,40 @@ add.addEventListener("click", e => {
     time.classList.add("todo-time");
     time.innerText = todoMonth + " / " + todoDate;
 
+    // create green check
+    let completeButton = document.createElement("button");
+    completeButton.classList.add("complete");
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    // add Event Listener
+    completeButton.addEventListener("click", event => {
+        let todoItem = event.target.parentElement;
+        todoItem.classList.toggle("done");
+    })
+
+    // create red trash canvas
+    let trashButton = document.createElement("button");
+    trashButton.classList.add("trash");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    // add Event Listener
+    trashButton.addEventListener("click", event => {
+        let todoItem = event.target.parentElement;
+        todoItem.style.animation = "scaleDown 0.3s forwards";
+        todoItem.addEventListener("animationend", () => {
+            todoItem.remove()
+        })
+    })
+
     // add text and time to addTodo
     addTodo.appendChild(text); addTodo.appendChild(time);
+    // add complete button and trash button
+    addTodo.appendChild(completeButton); addTodo.appendChild(trashButton);
+
+    // animation scaleUp here
+    addTodo.style.animation = "scaleUp 0.5s forwards";
+
+    // clear input
+    form.children[0].value = ""; form.children[1].value = ""; form.children[2].value = "";
+
     // add addTodo to section
     section.appendChild(addTodo);
 })
